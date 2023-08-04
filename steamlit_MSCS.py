6429744cc12a8d300fcc8ca2024d0d6a5a6c0967
bernstar <abernstein@noodle.com>
2023-08-04 00:18:46 -0400

    Initial commit

---
+++ steamlit_MSCS.py
@@ -0,0 +1,100 @@
+import logging
+import sys
+import time
+from typing import Optional
+import requests
+import streamlit as st
+from streamlit_chat import message
+
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(format=log_format, stream=sys.stdout, level=logging.INFO)
+
+BASE_API_URL = "http://127.0.0.1:7861/api/v1/process"
+FLOW_ID = "0f1feb39-df18-448f-a0a7-79c50342149e"
+# You can tweak the flow by adding a tweaks dictionary
+# e.g {"OpenAI-XXXXX": {"model_name": "gpt-4"}}
+TWEAKS = {
+  "OpenAIEmbeddings-3OTU2": {},
+  "Chroma-elGpI": {},
+  "ChatOpenAI-38h1l": {"model_name": "gpt-4"},
+  "ConversationalRetrievalChain-4FTbi": {},
+  "ConversationBufferMemory-YTFcZ": {}
+}
+BASE_AVATAR_URL = (
+    "https://raw.githubusercontent.com/garystafford-aws/static-assets/main/static"
+)
+
+
+def main():
+    st.set_page_config(page_title="UTK MSCS Chatbot")
+
+    st.markdown("##### UTK MSCS Chatbot")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"], avatar=message["avatar"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input("Ask me anything about UTK MSCS"):
+        # Add user message to chat history
+        st.session_state.messages.append(
+            {
+                "role": "user",
+                "content": prompt,
+                "avatar": f"{BASE_AVATAR_URL}/people-64px.png",
+            }
+        )
+        # Display user message in chat message container
+        with st.chat_message(
+            "user",
+            avatar=f"{BASE_AVATAR_URL}/people-64px.png",
+        ):
+            st.write(prompt)
+
+        # Display assistant response in chat message container
+        with st.chat_message(
+            "assistant",
+            avatar=f"{BASE_AVATAR_URL}/bartender-64px.png",
+        ):
+            message_placeholder = st.empty()
+            with st.spinner(text="Thinking..."):
+                assistant_response = generate_response(prompt)
+                message_placeholder.write(assistant_response)
+        # Add assistant response to chat history
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": assistant_response,
+                "avatar": f"{BASE_AVATAR_URL}/bartender-64px.png",
+            }
+        )
+
+
+def run_flow(inputs: dict, flow_id: str, tweaks: Optional[dict] = None) -> dict:
+    api_url = f"{BASE_API_URL}/{flow_id}"
+
+    payload = {"inputs": inputs}
+
+    if tweaks:
+        payload["tweaks"] = tweaks
+
+    response = requests.post(api_url, json=payload)
+    return response.json()
+
+
+def generate_response(prompt):
+    logging.info(f"question: {prompt}")
+    inputs = {"text": prompt}
+    response = run_flow(inputs, flow_id=FLOW_ID, tweaks=TWEAKS)
+    try:
+        logging.info(f"answer: {response['result']['text']}") 
+        return response["result"]["text"]  
+    except Exception as exc:
+        logging.error(f"error: {response}")
+        return "Sorry, there was a problem finding an answer for you."
+
+
+if __name__ == "__main__":
+    main()
